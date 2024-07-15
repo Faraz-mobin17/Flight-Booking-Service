b30b6186f5809f11830329d17e2d73628e116e68
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-15 20:15:26 +0530

    ✅removed info controller and added get route to info controller

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require("express");
 
-const { InfoController } = require('../../controllers');
+const BookingRoutes = require("./booking-route");
+const InfoRoutes = require("./info-route");
 
 const router = express.Router();
 
-router.get('/info', InfoController.info);
+router.get("/info", InfoRoutes);
+router.post("/bookings", BookingRoutes);
 
 module.exports = router;
