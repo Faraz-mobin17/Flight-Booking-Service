82e76c847d63a34ac94fa488302444b2acbcd7d6
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-16 18:18:08 +0530

    ✅ added booking repository

--- src/repositories/booking-repository.js
+++ src/repositories/booking-repository.js
@@ -5,6 +5,32 @@ class BookingRepository extends CrudRepository {
   constructor() {
     super(Booking);
   }
+
+  async createBooking(data, transaction) {
+    const response = await Booking.create(data, { transaction });
+    return response;
+  }
+
+  async get(data, transaction) {
+    const response = await this.model.findByPk(data, { transaction });
+    if (!response) {
+      throw new ApiError(StatusCodes.NOT_FOUND, "Booking not found");
+    }
+    return response;
+  }
+
+  async update(id, data, transaction) {
+    const response = await this.model.update(
+      data,
+      {
+        where: {
+          id: id,
+        },
+      },
+      { transaction }
+    );
+    return response;
+  }
 }
 
 module.exports = BookingRepository;
