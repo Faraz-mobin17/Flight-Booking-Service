11935487cf1944dc78bf4faa537f46aaceb5ce57
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-07-16 18:15:48 +0530

    ✅ added payemtn route

--- src/routes/v1/booking-route.js
+++ src/routes/v1/booking-route.js
@@ -2,7 +2,12 @@ const express = require("express");
 const router = express.Router();
 
 const { BookingController } = require("../../controllers");
-
+const { PaymentMiddleware } = require("../../middlewares");
 router.post("/", BookingController.createBooking);
+router.post(
+  "/payments",
+  PaymentMiddleware.validatePayment,
+  BookingController.makePayment
+);
 
 module.exports = router;
